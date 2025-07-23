913ec05decd84513d8f4fd2f70717278ef6a41f8
Serzh-byte <serzhtadevosyan1@gmail.com>
2025-07-23 23:40:10 +1000

    ooioji

--- server/server/models.py
+++ server/server/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 import uuid
 from storages.backends.s3boto3 import S3Boto3Storage
+from .storage import CustomS3Storage
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -119,8 +120,8 @@ class Flashcard(models.Model):
     folder = models.ForeignKey(FlashcardFolder, on_delete=models.CASCADE, related_name='flashcards')
     question = models.TextField(blank=True)
     answer = models.TextField(blank=True)
-    question_image = models.ImageField(upload_to='flashcard_images/', null=True, blank=True, storage=S3Boto3Storage())
-    answer_image = models.ImageField(upload_to='flashcard_images/', null=True, blank=True, storage=S3Boto3Storage())
+    question_image = models.ImageField(upload_to='flashcard_images/', null=True, blank=True, storage=CustomS3Storage())
+    answer_image = models.ImageField(upload_to='flashcard_images/', null=True, blank=True, storage=CustomS3Storage())
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
